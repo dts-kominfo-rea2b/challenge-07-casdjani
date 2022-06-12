ea90cd27cb2f731e5e0fe5eec86b5d65136721d2
casdjani <nazira.andjani@gmail.com>
2022-06-12 22:28:21 +0930

    challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async(emosi) => {
+  try{
+    const listFilm = await (await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])).flat()
+    emosi = listFilm.filter(item => item.hasil === emosi);
+    return emosi.length;
+  }
+  catch(rejectMessage){
+    console.log(`Error in: ${rejectMessage}`);
+  }
+};
 
 module.exports = {
   promiseOutput,
